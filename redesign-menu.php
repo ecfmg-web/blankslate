053745afc764a6f622013eaee8d4f4e4c59cc6c8
Guy Radcliffe <gradcliffe@ecfmg.org>
2022-02-18 13:05:24 -0500

    parent styles for css and removed breadcrumbs from the menu file

--- redesign-menu.php
+++ redesign-menu.php
@@ -48,32 +48,7 @@
     <div class="container">
       <div class="columns">
         <div class="column is-full">
-          <?php
-          // if on the news landing page, hard code nav ul li with NO POST TITLE in the breadcrumb
-          if ($_SERVER['REQUEST_URI']==='/maintenance/') { ?>
-          <nav class='breadcrumb' aria-label='breadcrumbs'>
-              <ul>
-                <li><a href='/'>Home</a></li>
-                <li class='is-active'><a href='/maintenance/' aria-current='page'>News</a></li>
-              </ul>
-          </nav>
-          <?php
-          } elseif (strpos($_SERVER['REQUEST_URI'],'maintenance/')) {
-          // if news AND on a single post, hard code nav ul li elements WITH POST TITLE in the breadcrumb
-            ?>
-            <nav class='breadcrumb' aria-label='breadcrumbs'>
-              <ul>
-                <li><a href='/'>Home</a></li>
-                <li><a href='/maintenance/'>News</a></li>
-                <li class='is-active'>
-                  <a href='#' aria-current='page'><?php the_title(); ?></a>
-                </li>
-              </ul>
-          </nav>
-          <?php // if NOT NEWS, use controller files $breadcrumb
-          } else {
-            echo $breadcrumb;
-          } ?>
+          <?php echo $breadcrumb; ?>
         </div>
       </div>
       <div class="columns">
