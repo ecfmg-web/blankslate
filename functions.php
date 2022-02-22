c537f1523f2b50e026af15f34d459fd900d3d795
Guy Radcliffe <gradcliffe@ecfmg.org>
2022-02-22 11:38:23 -0500

    new code for functions

--- functions.php
+++ functions.php
@@ -69,12 +69,9 @@ function blankslate_widgets_init() {
     register_sidebar( array(
     'name' => esc_html__( 'Sidebar Widget Area', 'blankslate' ),
     'id' => 'primary-widget-area',
-    'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-    'after_widget' => '</li>',
+    'before_widget' => '<section id="%1$s" class="widget-container %2$s">',
+    'after_widget' => '</section>',
     'before_title' => '<h3 class="widget-title">',
     'after_title' => '</h3>',
     ) );
 }
-<script>
-    document.querySelector("#archives-2>ul").outerHTML = "<select id='archives-2'>";
-</script>
