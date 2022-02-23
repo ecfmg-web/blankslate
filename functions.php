e3deb3af61ca904b3f96e7f159bbff61effa75e2
Guy Radcliffe <gradcliffe@ecfmg.org>
2022-02-23 10:20:25 -0500

    sidebar styles

--- functions.php
+++ functions.php
@@ -95,3 +95,14 @@ function wpb_recentposts_dropdown() {
 
     echo $string;
     }
+
+    function html5_search_form( $form ) {
+        $form = '<section class="search"><form role="search" method="get" id="search-form" class="search-form" action="' . home_url( '/' ) . '" >
+       <label class="screen-reader-text" for="s">' . __('',  'domain') . '</label>
+        <input type="search" class="search-field" value="' . get_search_query() . '" name="s" id="s" placeholder="Search News" />
+        <input type="submit" id="searchsubmit" class="search-submit" value="'. esc_attr__('Search News', 'domain') .'" />
+        </form></section>';
+        return $form;
+   }
+
+    add_filter( 'get_search_form', 'html5_search_form' );
