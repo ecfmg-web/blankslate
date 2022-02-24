d7856672d66bb9b9691cfe3f17beb4002a5a5841
Guy Radcliffe <gradcliffe@ecfmg.org>
2022-02-24 14:50:52 -0500

    remove style.css from header, update logo in the menu

--- redesign-menu.php
+++ redesign-menu.php
@@ -1,61 +1,45 @@
 <section class="section logoSection">
-    <div class="container">
-      <div class="columns is-gapless">
-        <div class="column is-one-fifth">
-          <div class="content mobileCenter">
-            <a href="/"
-              ><img src="/images/intealth-ecfmg.png" alt="Intealth ECFMG"
-            /></a>
-          </div>
+  <div class="container">
+    <div class="columns is-vcentered">
+      <div class="column is-one-third">
+        <div class="content mobileCenter">
+          <a href="/"><img src="/images/intealth-ecfmg-redesign.png" alt="Intealth ECFMG" /></a>
         </div>
-        <div class="column is-one-third">
-          <div class="content">
-            <p>Educational Commission for Foreign Medical Graduates</p>
-          </div>
+      </div>
+      <div class="column is-one-third">
+        <div class="content is-medium">
+          <p><b>Educational Commission for Foreign Medical Graduates</b></p>
         </div>
-        <div class="addSearchWrapper column is-two-fifths">
-          <span class="is-pulled-right">
-            <form
-              method="get"
-              action="https://www.faimer.org/addsearch-results.php"
-            >
-              <input
-                type="text"
-                name="addsearch"
-                placeholder="Search ..."
-                class="addsearch"
-                title="addsearch"
-              />
+      </div>
+      <div class="addSearchWrapper column is-two-fifths">
+        <span class="is-pulled-right">
+          <form method="get" action="https://www.ecfmg.org/addsearch-results2.php">
+            <label for="addSearchInput" class="is-sr-only">Search</label><input type="text" id="addSearchInput" name="addsearch" placeholder="Search ..." class="addsearch" title="addsearch" />
 
-              <input
-                type="submit"
-                class="addsearchResultsSubmit button is-primary is-small"
-                title="addsearch"
-                value="Search"
-              />
-            </form>
-          </span>
-        </div>
+            <input type="submit" class="addsearchResultsSubmit button is-primary is-small" title="addsearch" value="Search" />
+          </form>
+        </span>
       </div>
     </div>
-  </section>
-  <section class="section topSection">
-    <div class="container">
-      <?php include 'redesign-navbar.php';?>
+  </div>
+</section>
+<section class="section topSection">
+  <div class="container">
+    <?php include 'redesign-navbar.php'; ?>
+  </div>
+</section>
+<section class="section has-background-secondary p-4">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-full">
+        <?php echo $breadcrumb; ?>
+      </div>
     </div>
-  </section>
-  <section class="section has-background-secondary p-4">
-    <div class="container">
-      <div class="columns">
-        <div class="column is-full">
-          <?php echo $breadcrumb; ?>
+    <div class="columns has-heading-menu">
+      <div class="column flexAuto">
+        <div class="content">
+          <h2 class="h1 brandIntro"><?php echo $pageHeading2; ?></h2>
         </div>
       </div>
-      <div class="columns">
-        <div class="column flexAuto">
-          <div class="content">
-            <h2 class="h1 brandIntro"><?php echo $pageHeading2; ?></h2>
-          </div>
-        </div>
-        <div class="column flexAuto">
-          <div class="content">
+      <div class="column flexAuto">
+        <div class="content">
