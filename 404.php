86d42cc97c1ff695e52e0989898a0000bf8d0d80
Guy Radcliffe <gradcliffe@ecfmg.org>
2022-02-25 14:07:01 -0500

    added 404

--- 404.php
+++ 404.php
@@ -1,13 +1,33 @@
-<?php get_header(); ?>
-<main id="content" role="main">
-<article id="post-0" class="post not-found">
-<header class="header">
+<?php
+// echo $_SERVER['REQUEST_URI'];
+$title="ECFMG News | ";
+$pageHeading2 = "News";
+$sidebar = "sidebar";
+$description = "";
+include ($_SERVER['DOCUMENT_ROOT']) . "/redesign-header.php";
+include ($_SERVER['DOCUMENT_ROOT']) . "/redesign-menu.php"; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="container">
+    <div class="columns">
+      <div class="column is-four-fifths">
+        <div class="content is-medium">
 <h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Not Found', 'blankslate' ); ?></h1>
-</header>
+
 <div class="entry-content" itemprop="mainContentOfPage">
 <p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
 <?php get_search_form(); ?>
 </div>
-</article>
-</main>
-<?php get_footer(); ?>
+</div><!-- content-->
+</div><!-- column is-four-fifths -->
+<div class="column is-one-fifth">
+<?php get_sidebar(); ?>
+</div><!-- column is-one-fifth -->
+</div><!-- columns -->
+</section>
+<?php
+include ($_SERVER['DOCUMENT_ROOT']) . "/redesign-footer.php";
