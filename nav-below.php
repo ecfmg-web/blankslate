7e40024b84113b3a01fd1af1df8bc66c5f1b6ed0
Guy Radcliffe <gradcliffe@ecfmg.org>
2022-02-26 19:18:32 -0500

    fixed older newer nav below posts links

--- nav-below.php
+++ nav-below.php
@@ -1,5 +1,5 @@
 <?php $args = array(
-'prev_text' => sprintf( esc_html__( '%s older', 'blankslate' ), '<span class="meta-nav">&larr;</span>' ),
-'next_text' => sprintf( esc_html__( 'newer %s', 'blankslate' ), '<span class="meta-nav">&rarr;</span>' )
+'prev_text' => sprintf( esc_html__( 'older %s', 'blankslate' ), '<span class="meta-nav">&rarr;</span>' ),
+'next_text' => sprintf( esc_html__( '%s newer', 'blankslate' ), '<span class="meta-nav">&larr;</span>' )
 );
 the_posts_navigation( $args );
